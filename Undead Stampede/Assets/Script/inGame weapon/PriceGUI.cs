6f6d6cf99a6c26204e675b0d7f5fcb4b84a1ee0f
krisnasambora <krisnasambora@yahoo.com>
2014-07-23 13:46:27 +0700

    weapon buttons

--- Undead Stampede/Assets/Script/inGame weapon/PriceGUI.cs
+++ Undead Stampede/Assets/Script/inGame weapon/PriceGUI.cs
@@ -4,13 +4,18 @@ using System.Collections;
 public class PriceGUI : MonoBehaviour {
 	private Vector3 textPos;
 	public string priceText;
+	private GUIStyle style = new GUIStyle();
+	public Font guiFont;
 
 	// Use this for initialization
 	void Start () {
+		style.font = guiFont;
+		style.normal.textColor = Color.green;
+		style.hover.textColor = Color.green;
 		textPos = Camera.main.WorldToScreenPoint (transform.position);
 	}
 
 	void OnGUI(){
-		GUI.Label(new Rect(textPos.x - 15,textPos.y + 505,200,20), priceText);
+		GUI.Label(new Rect(textPos.x - 15,textPos.y + 511,200,20), priceText, style);
 	}
 }
